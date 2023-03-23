1642f38261653c21373d9c4a3b4d8b4ccd8b86c1
sandrbuz <sandrbuz@mail.ru>
2023-03-23 14:51:26 +0300

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return  <div className='content'>
+    <div>
+      <img src='https://t3.ftcdn.net/jpg/05/48/56/38/360_F_548563894_s5tGFJjPhc7lp5uG4iJkR77QbgvrKr9e.jpg' />
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      My posts
+      <div>
+        new post
+      </div>
+      <div>
+        <div>
+          post 1
+        </div>
+        <div>
+          post 2
+        </div>
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile;
