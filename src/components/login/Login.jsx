a956cbd4cbe803ebac2d03461767c701c1e0c62e
sandrbuz <sandrbuz@mail.ru>
2023-10-27 22:30:53 +0300

    Additionally, I added a redirect from /login to /profile (if isAuth===true). So I will remain on /profile when the application is initially loaded. (Left the solution commented out)

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
 const Login = (props) => {
+
+//  if(props.isAuth===true){return <Navigate to='/profile'/>}
+
+
    return (
      <div><h1>Login</h1></div>
     )
 }
 
+// const mapStateToProps = (state) => {
+//   return {
+//       isAuth: state.auth.isAuth,
+//   }
+// }
+
+// export default connect(mapStateToProps,{})(Login);
 export default Login;
