db3b6b47b3744feb1e3b4182f125be7066a6e9a1
sandrbuz <sandrbuz@mail.ru>
2023-12-10 18:25:19 +0200

    Lesson 75 Created redux-form. But we haven’t connected it with the API yet

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,13 +1,39 @@
 import React from "react";
+import { reduxForm, Field } from "redux-form";
+
+const LoginForm = (props) => {
+  //  if(props.isAuth===true){return <Navigate to='/profile'/>}
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <Field placeholder={"login"} name={"login"} component={"input"} type={"text"} />
+      </div>
+      <div>
+        <Field placeholder={"password"} name={"password"} component={"input"} type={"text"}/>
+      </div>
+      <div>
+        <Field component={"input"} name={"rememberMe"}  type={"checkbox"}/> remember me
+      </div>
+      <div>
+        <button type={"submit"}>login</button>
+      </div>
+    </form>
+  )
+}
+
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm);
 
 const Login = (props) => {
+   const onSubmit = (formData) => { //object(formData) with all input values
+      console.log(formData)
+  }
 
-//  if(props.isAuth===true){return <Navigate to='/profile'/>}
-
-
-   return (
-     <div><h1>Login</h1></div>
-    )
+  return (
+    <div>
+      <h1>Login</h1>
+      <LoginReduxForm onSubmit={onSubmit} />
+    </div>
+  )
 }
 
 // const mapStateToProps = (state) => {
