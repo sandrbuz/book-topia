b4af4b3dbf3993472b365d9c53254408bf18f79a
sandrbuz <sandrbuz@mail.ru>
2024-02-26 18:36:13 +0200

    I slightly completed the adaptive for UsersPage and Header

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -83,6 +83,7 @@ const Users = ({ currentPage, totalUsersCount, pageSize, onPageChanged, ...props
         position: 'absolute',
         top: '50%',
         left: '50%',
+        // top: '60%',
         transform: 'translate(-50%, -50%)',
         zIndex: 9999,
     }
